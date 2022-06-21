b06b5425a67cb48a473eb44a33b0c8e6b575f320
Veronica Lemos <veulemos@gmail.com>
2022-06-21 00:54:26 -0300

    Modulos JS

---
+++ Curso-JS/BasecampJs/Atividade/Modulos/main.mjs
@@ -0,0 +1,5 @@
+import {mostraIdade, mostraCidade, mostraHobby} from './funcoes.mjs';
+
+console.log(mostraIdade('Veronica', 37));
+console.log(mostraCidade('Veronica', 'Salvador'));
+console.log(mostraHobby('Veronica', 'Jogar PS4 e Roblox'));
